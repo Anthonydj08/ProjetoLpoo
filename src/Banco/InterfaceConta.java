3dd23d07220451468b048d2883b728a095b9aa7e
MauricioSantos100 <43189404+MauricioSantos100@users.noreply.github.com>
2020-08-26 18:01:19 -0300

    adicinado imports na interfaceConta

--- src/Banco/InterfaceConta.java
+++ src/Banco/InterfaceConta.java
@@ -1,8 +1,11 @@
 package Banco;
 
+import Exceptions.SaldoException;
+import Exceptions.SaqueNegativoException;
+
 public interface InterfaceConta {
 	
 	public void realizadeposito(double deposito);
-	public void realizasaque(double saque);
+	public void realizasaque(double saque) throws SaldoException, SaqueNegativoException;
 
 }
